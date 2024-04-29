ff6da5211392fe7a3fe3b1bac9daf8d60988d562
pete382 <ivan382@hotmail.com>
2024-04-29 10:28:03 -0500

    first committ

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Getting input from user
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Enter the radius");
+string entry = Console.ReadLine();
+Console.WriteLine(entry);
+
+//int result = int.Parse(entry);
+//Console.WriteLine(result);
+
+float resulte = float.Parse(Console.ReadLine());
+double AREA = Math.PI * resulte*resulte;
+
+Console.WriteLine(AREA);
+
+Console.WriteLine();
+
+
+string name = "Pedro Rueda Diaz"
